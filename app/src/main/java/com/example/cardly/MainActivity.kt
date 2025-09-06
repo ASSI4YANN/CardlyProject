ba266af9184fadf736c6dda5627c6ed84ee0c969
ASSI4YANN <nassiyannjunior@gmail.com>
2025-09-06 14:24:08 +0000

    modif

--- app/src/main/java/com/example/cardly/MainActivity.kt
+++ app/src/main/java/com/example/cardly/MainActivity.kt
@@ -17,9 +17,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
